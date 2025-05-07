dec9f87295f858b2ec3f8edd2aff2cc8c85478ad
mgorkha <menakshi@gmail.com>
2025-05-07 21:05:29 +0530

    Assignment completed

---
+++ task1.py
@@ -0,0 +1,7 @@
+n1=float(input("Enter the First Number: "))
+n2=float(input("Enter the Second Number: "))
+
+print("Addition: ", n1 + n2)
+print("Subtraction: ", n1 - n2)
+print("Multiplication: ", n1 * n2)
+print("Division: ", n1 / n2)
